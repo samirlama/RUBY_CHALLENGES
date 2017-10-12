64eab18cb37587b9ce0cb42a0cb198ea94a95a4e
samirlama <samirlama509@gmail.com>
2017-10-12 18:40:31 +0545

    Model created

---
+++ db/migrate/20171012103310_create_ideas.rb
@@ -0,0 +1,10 @@
+class CreateIdeas < ActiveRecord::Migration[5.1]
+  def change
+    create_table :ideas do |t|
+
+      t.timestamps null :false
+      t.text :description
+      t.string :author
+    end
+  end
+end
