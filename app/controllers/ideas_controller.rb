aad8ff71affbd9f6dfffe6315bba05cb108364ac
samirlama <samirlama509@gmail.com>
2017-10-20 16:04:24 +0545

    Validation added on form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -4,12 +4,29 @@ class IdeasController < ApplicationController
 	end	
 	def create
 		@ideas = Idea.create(idea_params)
+		if @ideas.valid?
+			#implemeant later
+		else
+			#implement later
+		end		
 		redirect_to root_path
 
+	end	
+	def edit
+		@ideas = Idea.find(params[:id])
+	end	
+	def update
+		@ideas = Idea.find(params[:id])
+		if @ideas.update(idea_params)
+			redirect_to root_path
+		else
+			redirect_to edit_idea_path
+		end		
 	end	
 	private
 	def idea_params
 		params.require(:idea).permit(:description,:author)
 
 	end	
+	
 end
